52f2ea9098717603434a621489d4a7970786be69
leonardoaraujo.santos <leonardoaraujo.santos@4d1706e5-948e-466b-b837-005b0b4bcd27>
2012-05-24 18:52:20 +0000

    Adding bit files for quick test (Also the MCS) and some docs

--- hdl/iseProject/top_hw_testbench.v
+++ hdl/iseProject/top_hw_testbench.v
@@ -30,10 +30,13 @@ module top_hw_testbench(
 	wire trig_0;
 	wire [16:0] data;
 	
+	// Declare clock DCM multiplier (3)
+	wire clock3x, clock1x, clockbuf;
+	
 	// Instantiate our Device under test
 	lcd_controller DUT (
 		rst,
-		clk,
+		clock1x,
 		rs_in,
 		data_in,
 		strobe_in,
@@ -45,6 +48,15 @@ module top_hw_testbench(
 		disable_flash,
 		done
 		);
+	
+	// Instantiate the module of clock multiplier		
+	 corePLL instance_name (
+    .CLKIN_IN(clk), 
+    .CLKFX_OUT(clock3x), 
+    .CLKIN_IBUFG_OUT(clockbuf), 
+    .CLK0_OUT(clock1x)
+    );
+	 
 	 
 	coreICON integratedController (
       .CONTROL0(control0), // INOUT BUS [35:0]
@@ -54,7 +66,7 @@ module top_hw_testbench(
 	
 	coreILA integratedLogicAnalyser (
       .CONTROL(control0), // INOUT BUS [35:0]
-      .CLK(clk), // IN
+      .CLK(clock3x), // IN
       .DATA(data), // DATA [16:0];
       .TRIG0(trig_0)
 	); // IN BUS [0:0]
@@ -62,7 +74,7 @@ module top_hw_testbench(
 	coreVIO VIO_inst
     (
       .CONTROL(control1), // INOUT BUS [35:0]
-		.CLK(clk),
+		.CLK(clock3x),
 		.SYNC_OUT(async_out)
       //.ASYNC_OUT(async_out)
 	); // IN BUS [18:0]
